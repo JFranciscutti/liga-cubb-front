c820ec3d381bd8994317caaf7f4ddc48d9094bb1
Joaquin Franciscutti <joaco.1999.13@gmail.com>
2024-10-10 11:04:38 -0300

    deploy

---
+++ dist/assets/enums-ec95615b.js
@@ -0,0 +1,1 @@
+var r=(a=>(a.MASCULINO="masculino",a.FEMENINO="femenino",a))(r||{});export{r as G};
--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/liga-cubb-front/sw.js', { scope: '/liga-cubb-front/' })})}
